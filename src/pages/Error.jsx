aad91c4ceb6bf98778032375dea2ff09861d4cd1
FedericoCU <fede.cdiaz@gmail.com>
2023-02-17 15:03:38 -0300

    Error.jsx modified

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -11,7 +11,7 @@ const Error = () => {
     <section className={darkTheme?'error-page section title-color-change':'error-page section'}>
       <div className="error-container">
         <h1>oops! Page not found...</h1>
-        <Link to='/' className='btn btn-primary'>Back home</Link>
+        <Link to='/cocktail-db' className='btn btn-primary'>Back home</Link>
       </div>
     </section>
   )
